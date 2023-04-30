dbbb556f1396dbb987fe4944cf09d0371ea9a8e8
Dreamwalker <aristojeff@gmail.com>
2023-04-30 22:11:40 +0900

    Update backend code

---
+++ backend/go.mod
@@ -0,0 +1,39 @@
+module main.go
+
+go 1.20
+
+require (
+	github.com/gofiber/fiber/v2 v2.44.0
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/projectdiscovery/gologger v1.1.8
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mholt/archiver v3.1.1+incompatible // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/ulikunitz/xz v0.5.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.46.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/djherbis/times.v1 v1.3.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/projectdiscovery/gologger"
+)
+
+type Response struct {
+	Code     int        `json:"code"`
+	Message  string     `json:"message"`
+	TestData []TestData `json:"items"`
+}
+
+type ResponseModel struct {
+	Code    int     `json:"code"`
+	Message string  `json:"message"`
+	Diary   []Diary `json:"items"`
+}
+
+// Field names should start with an uppercase letter
+type Diary struct {
+	Id        string `json:"id" xml:"id" form:"id"`
+	Uuid      string `json:"uuid"`
+	Note      string `json:"note" xml:"note" form:"note"`
+	Dt        string `json:"dt"`
+	Timestamp int    `json:"timestamp"`
+}
+
+type TestData struct {
+	Id      int
+	Name    string
+	Age     string
+	Address string
+}
+
+var (
+	myDB *sql.DB
+)
+
+func createTestTable() {
+	query := `
+		CREATE TABLE IF NOT EXISTS diary (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			uuid TEXT,
+			note TEXT,
+			dt TEXT,
+			timestamp INTEGER
+		);
+	`
+	if _, err := myDB.Exec(query); err != nil {
+		checkErr(err)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		gologger.Error().Msgf(err.Error())
+	}
+}
+
+func insertTestData(uuid string, note string, timestamp string) {
+	query := `INSERT INTO diary VALUES(NULL,?,?,?)`
+	if _, err := myDB.Exec(query, uuid, note, timestamp); err != nil {
+		checkErr(err)
+	}
+	gologger.Info().Msgf("데이터 삽입 완료!")
+}
+
+func insertDiaryItem(uuid string, note string, dt string, timestamp int) {
+	query := `INSERT INTO diary VALUES(NULL,?,?,?,?)`
+	if _, err := myDB.Exec(query, uuid, note, dt, timestamp); err != nil {
+		checkErr(err)
+	}
+	gologger.Info().Msgf("데이터 삽입 완료!")
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "mydb.db")
+	checkErr(err)
+	defer db.Close()
+	myDB = db
+
+	createTestTable()
+	// insertTestData("hello", "29", "서울시")
+
+	app := fiber.New()
+
+	app.Post("/diary", func(c *fiber.Ctx) error {
+		item := new(Diary)
+
+		if err := c.BodyParser(item); err != nil {
+			return err
+		}
+		log.Println(item.Id)
+		log.Println(item.Note)
+		// insertTestData(item.Id, "29", "서울시")
+		insertDiaryItem(item.Uuid, item.Note, item.Dt, item.Timestamp)
+		// return c.SendString("Completed")
+		return c.JSON(item)
+	})
+	app.Get("/diary", func(c *fiber.Ctx) error {
+		search := c.Query("search")
+		log.Println(search)
+		rows, err := myDB.Query("SELECT * FROM diary WHERE Date(dt) = Date(?)", search)
+
+		// rows, err := stm.Query(search)
+		// rows, err := myDB.Query("SELECT * FROM diary WHERE Date(dt) = Date(?)")
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		tmps := make([]Diary, 0)
+
+		for rows.Next() {
+			var result Diary
+			rows.Scan(&result.Id, &result.Uuid, &result.Note, &result.Dt, &result.Timestamp)
+			gologger.Info().Msgf("UUID : " + result.Uuid)
+			gologger.Info().Msgf("노트내용 : " + result.Note)
+			gologger.Info().Msgf("Datetime : " + result.Dt)
+			gologger.Info().Msgf("Timestamp : " + fmt.Sprintf("%v", result.Timestamp))
+			fmt.Println("===================================================================")
+			tmps = append(tmps, result)
+
+		}
+		return c.JSON(ResponseModel{200, strconv.Itoa(len(tmps)), tmps})
+
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+}
